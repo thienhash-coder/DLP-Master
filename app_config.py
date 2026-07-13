e27e0e845ef75ec531b019d4127e1297fd20b30b
Nhữ Đình hà <thienhash@gmail.com>
2026-07-13 09:25:34 +0700

    fix: update release check url to master branch

--- app_config.py
+++ app_config.py
@@ -3,4 +3,4 @@ APP_NAME = "DLP Master"
 CURRENT_VERSION = "2.0.1"
 
 # Đường dẫn đến file version.json trên GitHub của bạn để check update
-UPDATE_URL = "https://raw.githubusercontent.com/thienhash-coder/dlp-master/main/version.json"
+UPDATE_URL = "https://raw.githubusercontent.com/thienhash-coder/dlp-master/master/version.json"
